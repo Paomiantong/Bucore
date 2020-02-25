e64e857ec0096e5726add704bbaa06d1939f1df7
Artum <paomiantong@qq.com>
2020-02-25 16:54:40 +0800

    natives

--- System.h
+++ System.h
@@ -1,6 +1,16 @@
 #ifndef BC_SYSTEM_H
 #define BC_SYSTEM_H
 
+#ifdef _WIN32
+#define B_OS "windows"
+#elif __linux__ 
+#define B_OS "linux"
+#elif __APPLE__ 
+#define B_OS "macos"
+#elif __MACOSX__
+#define B_OS "osx"
+#endif
+
 #include <string>
 #ifdef __WIN32__
 #include <io.h>
--- Version.cpp
+++ Version.cpp
@@ -71,7 +71,7 @@ void Version::Lib_load(Value& data)
 {
 	for (auto& v : data.GetArray())
 	{
-			if (v.HasMember("extract")||!AllowChecker(v))//Check Allow
+			if (!AllowChecker(v))//Check Allow
 			{
 				continue;
 			}
@@ -84,23 +84,40 @@ void Version::Lib_load(Value& data)
 					tmp1[0][x]='/';
 			}
 			/*Native Part&Push Library*/
+			std::string classifiers_key;
 			if(tmp1[1].find("platform")!=std::string::npos&&v.HasMember("natives"))
 			{
-				//last=tmp1[1]+"-"+tmp1[2]+"-"+native;
+				classifiers_key =  v["natives"][B_OS].GetString();
+				///* TODO: Add arch <25-02-20, yourname> */
+				if(classifiers_key.find("${arch}"))
+				{
+					Replace(classifiers_key,"${arch}","32",false);
+				}
+				last=tmp1[1]+"-"+tmp1[2]+"-"+classifiers_key;
 			}else
 				last=tmp1[1]+"-"+tmp1[2];
 			names=_cwd_+"/.minecraft/libraries/"+tmp1[0]+"/"+tmp1[1]+"/"+tmp1[2]+"/"+last+".jar";
 			if(v.HasMember("downloads"))
 			{
-				Value& downloads=v["downloads"]["artifact"];
-				libraries.Add(names,downloads["url"].GetString(),false,downloads["size"].GetInt());
-			}else if(v.HasMember("url"))//todo:forge url
-			{
-				libraries.Add(names,v["url"].GetString(),false,-1);
-			}else
-			{
-				libraries.Add(names,"null",false,-1);
+				if(v["downloads"].HasMember("classifiers"))
+				{
+					Value& downloads=v["downloads"]["classifiers"][classifiers_key.c_str()];
+					libraries.Add(names,downloads["url"].GetString(),false,downloads["size"].GetInt());
+				}
+				else
+				{
+					Value& downloads=v["downloads"]["artifact"];
+					libraries.Add(names,downloads["url"].GetString(),false,downloads["size"].GetInt());
+				}
 			}
+			else if(v.HasMember("url"))//todo:forge url
+				{
+					libraries.Add(names,v["url"].GetString(),false,-1);
+				}
+				else
+				{
+					libraries.Add(names,"null",false,-1);
+				}
 	}
 }
 
@@ -126,7 +143,7 @@ bool AllowChecker(Value &val)
 				if(!rule.HasMember("os"))
 					allowed=(action=="allow");
 				else
-					if (rule["os"]["name"].GetString()=="windows")
+					if (rule["os"]["name"].GetString()==B_OS)
 						allowed=(action=="allow");
 			}
 			return allowed;
